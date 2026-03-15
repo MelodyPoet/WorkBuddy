838ef87863a4240c10792427691baa9a4128edb9
WorkBuddy <workbuddy@local.dev>
2026-03-15 12:55:43 -0700

    refactor: split CSS/JS into separate files (css/main.css, js/app.js)

---
+++ _commit.bat
@@ -0,0 +1,3 @@
+@echo off
+git -C "c:/Users/admin/WorkBuddy/20260314211855" add .
+git -C "c:/Users/admin/WorkBuddy/20260314211855" commit -m "refactor: split CSS/JS into separate files (css/main.css, js/app.js)"
