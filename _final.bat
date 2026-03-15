1361bf1efa3101e7744afe483538ee214c33f3d7
WorkBuddy <workbuddy@local.dev>
2026-03-15 12:56:13 -0700

    chore: remove _push.bat

--- _final.bat
+++ _final.bat
@@ -1,4 +1,4 @@
 @echo off
 git -C "c:/Users/admin/WorkBuddy/20260314211855" add -A
-git -C "c:/Users/admin/WorkBuddy/20260314211855" commit -m "chore: remove temp build scripts"
+git -C "c:/Users/admin/WorkBuddy/20260314211855" commit -m "chore: remove _push.bat"
 git -C "c:/Users/admin/WorkBuddy/20260314211855" push
