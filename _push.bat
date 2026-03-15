7bf954cc09be57c2da3405acf2db461f5ef3d1ec
WorkBuddy <workbuddy@local.dev>
2026-03-15 12:55:58 -0700

    chore: remove temp build scripts

---
+++ _push.bat
@@ -0,0 +1,4 @@
+@echo off
+git -C "c:/Users/admin/WorkBuddy/20260314211855" add -A
+git -C "c:/Users/admin/WorkBuddy/20260314211855" commit -m "chore: remove temp build scripts"
+git -C "c:/Users/admin/WorkBuddy/20260314211855" push
